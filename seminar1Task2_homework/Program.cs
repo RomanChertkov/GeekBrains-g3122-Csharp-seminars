7d2339bf2e5e21fb49830ec0d3ecb6b0a5598da9
Roman Chertkov <r.chertkov@mail.ru>
2022-09-01 19:50:06 +0700

    Added task 2. Homework

---
+++ seminar1Task2_homework/Program.cs
@@ -0,0 +1,17 @@
+﻿//=============================================================================
+// Напишите программу, которая на вход принимает два числа 
+// и выдаёт, какое число большее, а какое меньшее.
+//=============================================================================
+
+Console.WriteLine("Найти максимальное из 2-х чисел");
+
+Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine() ?? "");
+
+if (firstNumber > secondNumber) 
+    Console.WriteLine("Число " + firstNumber + " больше числа " +secondNumber);
+else 
+    Console.WriteLine("Число " + secondNumber + " больше числа " +firstNumber);
