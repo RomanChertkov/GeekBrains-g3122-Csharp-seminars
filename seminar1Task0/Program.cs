ce23ef3597b18f4bf6a68e964334a47299525889
Roman Chertkov <r.chertkov@mail.ru>
2022-09-01 13:45:33 +0700

    added task 0

---
+++ seminar1Task0/Program.cs
@@ -0,0 +1,15 @@
+﻿//=========================================================================
+// Напишите программу, которая на вход принимает число и выдаёт его квадрат 
+// (число умноженное на само себя).
+//=========================================================================
+
+Console.WriteLine("Возведение числа в квадрат");
+Console.Write("Введите число: ");
+
+string inputNumber = Console.ReadLine()??"";
+
+int number = int.Parse(inputNumber);
+// int squareNumber  = number * number;
+int squareNumber = (int)Math.Pow(number, 2);  
+
+Console.WriteLine("Квадрат числа " + inputNumber + " = " + squareNumber);
