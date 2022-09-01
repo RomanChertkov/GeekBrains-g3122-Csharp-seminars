edaa9cc63333e6a2dcd36d788488f11ffa1ba025
Roman Chertkov <r.chertkov@mail.ru>
2022-09-01 14:42:09 +0700

    added task 1

---
+++ seminar1Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿//=============================================================================
+//Напишите программу, которая на вход принимает два числа и проверяет,
+// является ли первое число квадратом второго
+//=============================================================================
+
+Console.WriteLine("Является ли первое число квадратом второго?");
+
+Console.WriteLine("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine() ?? "");
+
+string compareResult =
+    Math.Sqrt(firstNumber) == secondNumber ? " является" : " не является";
+
+Console.WriteLine(
+    "Первое число " + firstNumber + compareResult +
+    " квадратом второго числа " + secondNumber
+);
