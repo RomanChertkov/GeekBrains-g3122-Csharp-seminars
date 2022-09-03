b20315e52e2d87c450d53c18fc0b2fb7918f6c20
Roman Chertkov <r.chertkov@mail.ru>
2022-09-04 01:40:33 +0700

    Added tasks 13. Homework

---
+++ seminar2Task13_homework/Program.cs
@@ -0,0 +1,46 @@
+﻿//=============================================================================
+//                                     Задача 14
+// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+//=============================================================================
+
+
+Console.Clear();
+Console.WriteLine(
+    "Программа выводит третью цифру заданного числа "+
+    "или сообщает, что третьей цифры нет.\n"
+);
+
+string number;
+char thirdDigit = '0';
+bool checkResult = false;
+
+ReadData();
+CalculateData();
+PrintData();
+
+// Считывает число от пользователя
+void ReadData () {
+    Console.Write("Введите число: ");
+    number = Console.ReadLine() ?? "";
+}
+
+// Находит 3 цифру
+void CalculateData(){
+    if ( number.Length >= 3)
+    {
+    char[] digitArray = number.ToCharArray();
+    
+        thirdDigit = digitArray[2];
+        checkResult = true;
+    } 
+}
+
+//Выводит результат в консоль
+void PrintData(){
+    Console.WriteLine(
+        checkResult 
+        ? $"3 цифра числа {number} = {thirdDigit}"
+        : $"Третьей цифры нет"
+    );
+}
---
+++ seminar2Task13_homework_extra/Program.cs
@@ -0,0 +1,55 @@
+﻿//=============================================================================
+//                                     Задача 14
+// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет. (Решить для числа int)
+//=============================================================================
+
+
+Console.Clear();
+Console.WriteLine(
+    "Программа выводит третью цифру заданного числа "+
+    "или сообщает, что третьей цифры нет.\n"
+);
+
+int number;
+int thirdDigit = 0;
+bool checkResult = false;
+int digitCounter = 0;
+int digitPosition = 3; 
+
+ReadData();
+CalculateData();
+PrintData();
+
+// Считывает число от пользователя
+void ReadData () {
+    Console.Write("Введите число: ");
+    number = int.Parse(Console.ReadLine() ?? "");
+}
+
+// Находит 3 цифру
+void CalculateData(){
+
+    if (number > 99 )
+    {
+        int number2 = number;
+        // Подсчет количества разрядов в числе
+        while (number2 > 0){
+            number2/=10;
+            digitCounter++;
+        }
+        // обрезаем не нужные разряды до позиции 3 и берём последнюю цифру
+        thirdDigit =( number / (int) Math.Pow(10,digitCounter-digitPosition)) % 10;
+
+        checkResult = true;
+    }
+}
+
+//Выводит результат в консоль
+void PrintData(){
+    Console.WriteLine(
+        checkResult 
+        ? $"3 цифра числа {number} = {thirdDigit}"
+        : $"Третьей цифры нет"
+    );
+}
