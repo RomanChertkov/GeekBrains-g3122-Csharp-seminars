0bc1cbf6a687271cf3e7e492b6020999c0c5876d
Roman Chertkov <r.chertkov@mail.ru>
2022-09-04 12:50:11 +0700

    Added solving the task with Log10

--- seminar2Task13_homework_extra/Program.cs
+++ seminar2Task13_homework_extra/Program.cs
@@ -7,7 +7,7 @@
 
 Console.Clear();
 Console.WriteLine(
-    "Программа выводит третью цифру заданного числа "+
+    "Программа выводит третью цифру заданного числа " +
     "или сообщает, что третьей цифры нет.\n"
 );
 
@@ -15,40 +15,45 @@ int number;
 int thirdDigit = 0;
 bool checkResult = false;
 int digitCounter = 0;
-int digitPosition = 3; 
+int digitPosition = 3;
 
 ReadData();
 CalculateData();
 PrintData();
 
 // Считывает число от пользователя
-void ReadData () {
+void ReadData()
+{
     Console.Write("Введите число: ");
     number = int.Parse(Console.ReadLine() ?? "");
 }
 
 // Находит 3 цифру
-void CalculateData(){
+void CalculateData()
+{
 
-    if (number > 99 )
+    if (number > 99)
     {
-        int number2 = number;
+        // int number2 = number;
         // Подсчет количества разрядов в числе
-        while (number2 > 0){
-            number2/=10;
-            digitCounter++;
-        }
-        // обрезаем не нужные разряды до позиции 3 и берём последнюю цифру
-        thirdDigit =( number / (int) Math.Pow(10,digitCounter-digitPosition)) % 10;
+        // while (number2 > 0){
+        //     number2/=10;
+        //     digitCounter++;
+        // }
+        // // обрезаем не нужные разряды до позиции 3 и берём последнюю цифру
+        // thirdDigit =( number / (int) Math.Pow(10,digitCounter-digitPosition)) % 10;
+        digitCounter = (int)Math.Log10(number);
+        thirdDigit = (number / (int)Math.Pow(10, (digitCounter + 1) - digitPosition)) % 10;
 
         checkResult = true;
     }
 }
 
 //Выводит результат в консоль
-void PrintData(){
+void PrintData()
+{
     Console.WriteLine(
-        checkResult 
+        checkResult
         ? $"3 цифра числа {number} = {thirdDigit}"
         : $"Третьей цифры нет"
     );
