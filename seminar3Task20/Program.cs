210030054ddfecb2cd8f1754b3c9d29564fb593a
Roman Chertkov <r.chertkov@mail.ru>
2022-09-09 01:06:55 +0700

    Added task 20

---
+++ seminar3Task20/Program.cs
@@ -0,0 +1,64 @@
+﻿//=============================================================================
+//                       Задача 20
+// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D пространстве.
+//=============================================================================
+
+ProgramDescription(
+    "Программа принимает на вход координаты двух точек " +
+    "и находит расстояние между ними в 2D пространстве.\n"
+);
+
+int[] pointA ={
+    ReadNumberFromConsole("Введите координату x точки A: "),
+    ReadNumberFromConsole("Введите координату y точки A: ")
+
+};
+int[] pointB ={
+    ReadNumberFromConsole("Введите координату x точки B: "),
+    ReadNumberFromConsole("Введите координату y точки B: ")
+};
+
+
+PrintResultToConsole(
+   "Расстояние между точками" +
+   $"А({pointA[0]},{pointA[1]}) и B({pointB[0]},{pointB[1]}) " +
+   $"в 2D пространстве = {DistanceBetween2Points(pointA, pointB)}"
+);
+
+//Очистка консоли и вывод описания работы программы
+void ProgramDescription(string text)
+{
+    Console.Clear();
+    Console.WriteLine(text);
+}
+
+
+// Получение числа из консоли
+int ReadNumberFromConsole(string call2ActionText)
+{
+    Console.Write(call2ActionText);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Вычисление расстояния между 2 точками
+double DistanceBetween2Points(int[] pointA, int[] pointB)
+{
+
+    int sum = 0;
+
+    for (int i = 0; i < pointA.Length; i++)
+    {
+        sum += (int)Math.Pow(pointB[i] - pointA[i], 2);
+    }
+
+    return Math.Round(Math.Sqrt(sum), 2);
+}
+
+
+//Вывод результата работы программы
+void PrintResultToConsole(string result)
+{
+    Console.WriteLine();
+    Console.WriteLine(result);
+}
