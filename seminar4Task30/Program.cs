3c08a8022b4d5dc1a2a55ba5eec98a58d0b00a8b
Roman Chertkov <r.chertkov@mail.ru>
2022-09-10 18:12:19 +0700

    Added task 30

---
+++ seminar4Task30/Program.cs
@@ -0,0 +1,49 @@
+﻿//=============================================================================
+//                       Задача 30
+// Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+//=============================================================================
+
+ProgramDescription(
+    "Напишите программу, которая выводит массив из 8 элементов, " +
+    "заполненный нулями и единицами в случайном порядке. \n"
+);
+
+Console.WriteLine("Для заполнения массива нажмите любую клавишу");
+Console.ReadKey(true);
+
+PrintResultToConsole(
+    "Массив из 8 случайных чисел (0 или 1) \n" +
+    $"[{string.Join(",", GenerateRandomArray(8))}]");
+
+
+// МЕТОДЫ
+
+int[] GenerateRandomArray(int arrayLenght)
+{
+    int[] randomArray = new int[arrayLenght];
+    Random randomNumber = new Random();
+
+    for (int i = 0; i < arrayLenght; i++)
+    {
+        randomArray[i] = randomNumber.Next(0, 2);
+    }
+
+    return randomArray;
+}
+
+
+
+//Очистка консоли и вывод описания работы программы
+void ProgramDescription(string text)
+{
+    Console.Clear();
+    Console.WriteLine(text);
+}
+
+//Вывод результата работы программы
+void PrintResultToConsole(string result)
+{
+    Console.WriteLine();
+    Console.WriteLine(result);
+}
