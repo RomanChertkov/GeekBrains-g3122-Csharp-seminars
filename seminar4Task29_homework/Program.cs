9d7b5bff3d704864df7b1b3452a5581265a73a6c
Roman Chertkov <r.chertkov@mail.ru>
2022-09-10 19:31:41 +0700

    Added task 29. Homework

---
+++ seminar4Task29_homework/Program.cs
@@ -0,0 +1,70 @@
+﻿//=============================================================================
+//                       Задача 29
+// Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран. Ввести с клавиатуры длину массива 
+// и диапазон значений элементов
+//=============================================================================
+
+ProgramDescription(
+    "Напишите программу, которая выводит массив из 8 элементов, " +
+    "и выводит их на экран. \n"
+);
+
+int arrayLenght = ReadNumberFromConsole("Ведите размер массива: ");
+
+int[] range =
+    {
+        ReadNumberFromConsole("Ведите начало диапазона случайных значений: "),
+        ReadNumberFromConsole("Ведите конец диапазона случайных значений: ")
+    };
+
+
+PrintResultToConsole(
+    $"Массив из {arrayLenght} случайных чисел в диапазоне " +
+    $"[{range[0]},{range[1]}] \n" +
+    $"[{string.Join(",", GenerateRandomArray(arrayLenght, range[0], range[1]))}]"
+);
+
+
+
+/*
+Метод заполняет массив случайными числами 
+в заданном диапазоне включая эти значения.
+Принимает длину будущего массива,
+начало и конец диапазона случайных значений 
+*/
+int[] GenerateRandomArray(int arrayLenght, int startRange, int finishRange)
+{
+    int[] randomArray = new int[arrayLenght];
+    Random randomNumber = new Random();
+
+    for (int i = 0; i < arrayLenght; i++)
+    {
+        randomArray[i] = randomNumber.Next(startRange, finishRange + 1);
+    }
+
+    return randomArray;
+}
+
+
+
+//Очистка консоли и вывод описания работы программы
+void ProgramDescription(string text)
+{
+    Console.Clear();
+    Console.WriteLine(text);
+}
+
+// Получение числа из консоли
+int ReadNumberFromConsole(string call2ActionText)
+{
+    Console.Write(call2ActionText);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Вывод результата работы программы
+void PrintResultToConsole(string result)
+{
+    Console.WriteLine();
+    Console.WriteLine(result);
+}
---
+++ seminar4Task29_homework_extra/Program.cs
@@ -0,0 +1,56 @@
+﻿//=============================================================================
+//                       Задача 29*
+// Написать программу которая из имен через запятую выберет случайное
+// имя и выведет в терминал
+//=============================================================================
+
+ProgramDescription(
+    "Напишите программу, которая из имен через запятую выберет случайное " +
+    "имя и выведет в терминал.\n"
+);
+
+string names = ReadStringFromConsole("Введите список имен через запятую: ");
+
+if (names.Equals(string.Empty))
+{
+    Console.WriteLine("Сегодня все останутся без пива :(");
+    Environment.Exit(0);
+}
+
+PrintResultToConsole(
+   $"Совершенно случайно за пивом бежит {RandomName(names)} :)"
+);
+
+
+// МЕТОДЫ
+
+// Метод парсит строку и выводит случайное имя
+string RandomName(string stringWithNames)
+{
+    string[] namesList = stringWithNames.Split(',');
+    return namesList[new Random().Next(0, namesList.Length)].Trim();
+}
+
+//Очистка консоли и вывод описания работы программы
+void ProgramDescription(string text)
+{
+    Console.Clear();
+    Console.WriteLine(text);
+}
+
+
+// Получение строки из консоли
+string ReadStringFromConsole(string call2ActionText)
+{
+    Console.Write(call2ActionText);
+    return (Console.ReadLine() ?? "").Trim();
+}
+
+
+
+//Вывод результата работы программы
+void PrintResultToConsole(string result)
+{
+    Console.WriteLine();
+    Console.WriteLine(result);
+}
