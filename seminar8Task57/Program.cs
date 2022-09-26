9c5006441d32149e0d2b5e20cdd3ac9dcfdab29b
Roman Chertkov <r.chertkov@mail.ru>
2022-09-26 13:56:12 +0700

    Added task 57

---
+++ seminar8Task57/Program.cs
@@ -0,0 +1,208 @@
+﻿//=============================================================================
+//                       Задача 57
+// Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, 
+// сколько раз встречается элемент входных данных.
+//=============================================================================
+
+
+bool endApp = false;
+
+while (!endApp)
+{
+    ProgramDescription(
+        "Программа выводит частотный словарь элементов двумерного массива.\n"
+    );
+    Console.WriteLine("Определение размера матрицы");
+    int matrixRow =
+        ValidateIntNumber(
+            ReadStringFromConsole("Введите количество строк в матрице : ")
+        );
+    int matrixColumn =
+        ValidateIntNumber(
+            ReadStringFromConsole("Введите количество столбцов в матрице: ")
+        );
+
+    Console.WriteLine("\nОпределение диапазона для генерации случайных чисел");
+
+    int startRange =
+        ValidateIntNumber(ReadStringFromConsole("Введите начало диапазона : "));
+    int endRange =
+        ValidateRangeEnd(
+            startRange,
+            ValidateIntNumber(
+                ReadStringFromConsole("Введите конец диапазона: ")
+            )
+    );
+
+    Console.WriteLine();
+    int[,] randomMatrix =
+        GenerateRandomMatrix((matrixRow, matrixColumn), (startRange, endRange));
+
+    Console.WriteLine("Исходная матрица");
+    PrintMatrix(randomMatrix);
+
+    PrintDictionary(FillFrequencyDictionary(randomMatrix));
+
+
+
+    ContinueProgram();
+}
+
+
+// Печать частотного словаря
+void PrintDictionary(SortedDictionary<int, int> dictionary)
+{
+
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine();
+
+    Console.WriteLine("Частота элементов:");
+    foreach (var item in dictionary)
+    {
+        Console.WriteLine("Элемент {0} количество повторов = {1}", item.Key, item.Value);
+    }
+
+    Console.ResetColor();
+}
+
+//Формирование частотного словаря
+SortedDictionary<int, int> FillFrequencyDictionary(int[,] matrix)
+{
+    SortedDictionary<int, int> frequencyDictionary =
+         new SortedDictionary<int, int>();
+
+    foreach (var item in matrix)
+    {
+        if (frequencyDictionary.ContainsKey(item))
+        {
+            frequencyDictionary[item]++;
+        }
+        else frequencyDictionary.Add(item, 1);
+    }
+
+    return frequencyDictionary;
+}
+
+//генерирует 2D массив
+int[,] GenerateRandomMatrix(
+    (int rows, int columns) matrixSize,
+    (int rangeStart, int rangeEnd) valuesRange
+
+    )
+{
+
+    int[,] matrix = new int[matrixSize.rows, matrixSize.columns];
+
+    for (int i = 0; i < matrixSize.rows; i++)
+    {
+        for (int j = 0; j < matrixSize.columns; j++)
+        {
+            matrix[i, j] = new Random()
+                    .Next(valuesRange.rangeStart, valuesRange.rangeEnd + 1);
+        }
+    }
+    return matrix;
+}
+
+
+// Вывод матрицы в консоль
+void PrintMatrix<T>(T[,] matrix)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+    Console.WriteLine();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+
+    Console.ResetColor();
+}
+
+// Метод проверяет ввел ли пользователь в консоли число
+// Если ввёл не число, то  метод просит ввести число
+// Возвращает число
+int ValidateIntNumber(string number)
+{
+    int cleanNumber = 0;
+    while (!int.TryParse(number, out cleanNumber))
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+
+        Console.WriteLine("Ошибка! Вы ввели не число.\n");
+
+        Console.ResetColor();
+        Console.Write("Введите целое число: ");
+
+        number = Console.ReadLine() ?? "";
+    }
+
+    Console.ResetColor();
+
+    return cleanNumber;
+
+}
+
+
+//валидация диапазона
+int ValidateRangeEnd(int startRange, int endRange)
+{
+
+    while (startRange > endRange)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine(
+            "Ошибка! " +
+            $"Конец диапазона должен быть больше начала ( > {startRange}).\n"
+        );
+
+        Console.ResetColor();
+        Console.Write("Введите конец диапазона ещё раз: ");
+
+        endRange =
+            ValidateIntNumber(ReadStringFromConsole(""));
+        Console.ResetColor();
+    }
+    return endRange;
+}
+
+
+//Метод зпрашивает у пользователя разрешение на выход или продолжение
+void ContinueProgram()
+{
+    //Изменение цвета вывода в консоль 
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+    Console.WriteLine();
+    Console.WriteLine(
+        "Для выхода из программы нажмите клавишу Escape (Esc). \n" +
+        "Для повторного запуска нажмите любую клавишу."
+    );
+
+    if (Console.ReadKey().Key == ConsoleKey.Escape) endApp = true;
+
+    Console.ResetColor();
+    Console.WriteLine();
+}
+
+
+//Очистка консоли и вывод описания работы программы
+void ProgramDescription(string text)
+{
+    Console.Clear();
+    Console.WriteLine(text);
+}
+
+
+// Получение строки из консоли
+string ReadStringFromConsole(string call2ActionText)
+{
+    Console.Write(call2ActionText);
+    return (Console.ReadLine() ?? "").Trim();
+}
