ff847c18d9f117e3ffcfed5aba09f8cf69c66cb3
Roman Chertkov <r.chertkov@mail.ru>
2022-09-28 13:25:54 +0700

    Added task 67

---
+++ seminar9Task67/Program.cs
@@ -0,0 +1,104 @@
+﻿//=============================================================================
+//                       Задача 67
+// Напишите программу, которая принимает на вход число N 
+// и выводит сумму его цифр.
+//=============================================================================
+
+
+bool endApp = false;
+
+while (!endApp)
+{
+    ProgramDescription(
+        "Программа принимает на вход число N принимает на вход число N "
+        + "и выводит сумму его цифр.\n"
+    );
+
+    int number =
+        ValidateIntNumber(ReadStringFromConsole("Введите число: "));
+
+    PrintResultToConsole($"Суммма цифр числа {number} = " + DigitSum(number));
+
+    ContinueProgram();
+}
+
+
+// Сусса чисел числа
+int DigitSum(int number)
+{
+    if (number == 0) return 0;
+    return number % 10 + DigitSum(number / 10);
+}
+
+
+// Метод проверяет ввел ли пользователь в консоли число
+// Если ввёл не число, то  метод просит ввести число
+// Возвращает число
+int ValidateIntNumber(string number)
+{
+    int cleanNumber = 0;
+    while (!int.TryParse(number, out cleanNumber))
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+
+        Console.WriteLine("Ошибка! Вы ввели не число.\n");
+
+        Console.ResetColor();
+        Console.Write("Введите целое число: ");
+
+        number = Console.ReadLine() ?? "";
+
+    }
+
+    Console.ResetColor();
+
+    return cleanNumber;
+
+}
+
+
+//Метод зпрашивает у пользователя разрешение на выход или продолжение
+void ContinueProgram()
+{
+    //Изменение цвета вывода в консоль 
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+    Console.WriteLine();
+    Console.WriteLine(
+        "Для выхода из программы нажмите клавишу Escape (Esc). \n" +
+        "Для повторного запуска нажмите любую клавишу."
+    );
+
+    if (Console.ReadKey().Key == ConsoleKey.Escape) endApp = true;
+
+    Console.ResetColor();
+    Console.WriteLine();
+}
+
+
+//Очистка консоли и вывод описания работы программы
+void ProgramDescription(string text)
+{
+    Console.Clear();
+    Console.WriteLine(text);
+}
+
+
+// Получение строки из консоли
+string ReadStringFromConsole(string call2ActionText)
+{
+    Console.Write(call2ActionText);
+    return (Console.ReadLine() ?? "").Trim();
+}
+
+
+//Вывод результата работы программы
+void PrintResultToConsole(string result)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+    Console.WriteLine();
+    Console.WriteLine(result);
+
+    Console.ResetColor();
+}
